d683b2e9c1ac50ef7612f2b55b7bb95f9c451a82
MightyBeast <zgpoter@gmail.com>
2021-12-15 23:59:34 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+var routes = require("./routes");
+
+const app = new express();
+const port = 3000;
+
+app.use("/", routes);
+
+app.set('view engine', 'ejs');
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
